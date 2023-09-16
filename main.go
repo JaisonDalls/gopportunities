650cff1a995fdb50fda441dfbdfdc550b9d2455c
Jaison Rodrigo Dallabrida <jaison.dallabrida@flowti.com.br>
2023-09-16 15:35:04 -0300

    feat: add custom logger.

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/JaisonDalls/gopportunities/router"
+import (
+	"github.com/JaisonDalls/gopportunities/config"
+	"github.com/JaisonDalls/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initializing the db config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//Initializing the routes
 	router.Initialize()
 }
