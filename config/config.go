a5d78be1e0cdc99ad113ea129b663d194507df6c
Jaison Rodrigo Dallabrida <jaison.dallabrida@flowti.com.br>
2023-09-21 08:16:18 -0300

    feat: initialized methods SQLite and Logger.

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -9,10 +11,22 @@ var (
 	logger *Logger
 )
 
+// Init is ...
 func Init() error {
+	var err error
+	db, err = InitializeSQLite()
+	if err != nil {
+		return fmt.Errorf("Error initializing Sqlite: %v", err)
+	}
 	return nil
 }
 
+// GetSQLite is ...
+func GetSQLite() *gorm.DB {
+	return db
+}
+
+// GetLogger is ...
 func GetLogger(p string) *Logger {
 	logger = NewLogger(p)
 	return logger
